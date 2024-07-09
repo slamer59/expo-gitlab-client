a3de927303a12bed3991f4833e27db07fe46e627
Pedot T <thomas.pedot@jokosun.com>
2024-07-09 11:29:24 +0200

    feat: Add static profile tempalte

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import React from 'react';
-import { Pressable, View } from 'react-native';
-
+import { Text } from '@/components/ui/text';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { useColorScheme } from '@/components/useColorScheme';
 import Colors from '@/constants/Colors';
+import { FontAwesome5 } from '@expo/vector-icons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Link, Tabs } from 'expo-router';
+import React from 'react';
+import { Image, Pressable, View } from 'react-native';
 
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
@@ -89,19 +90,37 @@ export default function TabLayout() {
         options={{
           title: 'Profile',
           tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+          headerTitle: () => (<View className="flex-row items-center mb-4">
+
+            <Image
+              source={{ uri: 'https://placehold.co/100x100' }}
+              style={{ width: 64, height: 64, borderRadius: 32 }}
+              className="mr-4"
+            />
+            <View>
+              <Text className="text-2xl font-bold ">Thomas PEDOT</Text>
+              <Text className="text-gray-400">slamer59</Text>
+            </View>
+          </View>),
           headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+            <View className="flex-row items-center mb-4">
+              <View className="flex-row ml-auto space-x-4">
+                <FontAwesome5 name="share-alt" size={24} color="black" />
+                <FontAwesome5 name="cog" size={24} color="black" />
+              </View>
+            </View>
+            // <Link href="/modal" asChild>
+            //   <Pressable>
+            //     {({ pressed }) => (
+            //       <FontAwesome
+            //         name="info-circle"
+            //         size={25}
+            //         color={Colors[colorScheme ?? 'light'].text}
+            //         style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+            //       />
+            //     )}
+            //   </Pressable>
+            // </Link>
           ),
         }}
       />
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,103 @@
+import { Text } from '@/components/ui/text';
+import { FontAwesome5 } from '@expo/vector-icons';
+import React from 'react';
+import { Image, View } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabTwoScreen() {
+export default function ProfileScreen() {
   return (
-    <View className="items-center justify-center flex-1">
-      <Text className="text-2xl font-bold">Tab Two</Text>
-      <View className="w-4/5 h-1 my-6 bg-gray-200 dark:bg-gray-700" lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+    <View className="flex-1 p-4">
+      <View className="mb-4">
+        <View className="flex-row items-center mb-2">
+          <FontAwesome5 name="map-marker-alt" size={24} color="gray" className="mr-2" />
+          <Text className="text-gray-400">Toulouse, FRANCE</Text>
+        </View>
+        <View className="flex-row items-center mb-2">
+          <FontAwesome5 name="envelope" size={24} color="gray" className="mr-2" />
+          <Text className="text-gray-400">thomas.pedot@gmail.com</Text>
+        </View>
+        <View className="flex-row items-center mb-2">
+          <FontAwesome5 name="user-friends" size={24} color="gray" className="mr-2" />
+          <Text className="text-gray-400">9 followers • 12 following</Text>
+        </View>
+        <View className="flex-row space-x-2">
+          <Image
+            source={{ uri: 'https://placehold.co/30x30' }}
+            style={{ width: 24, height: 24 }}
+          />
+          <Image
+            source={{ uri: 'https://placehold.co/30x30' }}
+            style={{ width: 24, height: 24 }}
+          />
+          <Image
+            source={{ uri: 'https://placehold.co/30x30' }}
+            style={{ width: 24, height: 24 }}
+          />
+          <Image
+            source={{ uri: 'https://placehold.co/30x30' }}
+            style={{ width: 24, height: 24 }}
+          />
+        </View>
+      </View>
+      <View className="mb-4">
+        <View className="flex-row items-center mb-2">
+          <FontAwesome5 name="thumbtack" size={24} color="gray" className="mr-2" />
+          <Text className="text-gray-400">Pinned</Text>
+        </View>
+        <View className="p-4 mb-2 rounded-lg bg-slate-100">
+          <View className="flex-row items-center mb-2">
+            <Image
+              source={{ uri: 'https://placehold.co/30x30' }}
+              style={{ width: 24, height: 24, borderRadius: 12, marginRight: 8 }}
+            />
+            <Text className="text-gray-400">slamer59</Text>
+          </View>
+          <Text className="mb-2 ">batch8_mednum</Text>
+          <View className="flex-row items-center text-gray-400">
+            <FontAwesome5 name="star" size={24} color="yellow" className="mr-1" />
+            <Text className="mr-4">0</Text>
+            <Text>Jupyter Notebook</Text>
+          </View>
+        </View>
+        <View className="p-4 rounded-lg bg-slate-100">
+          <View className="flex-row items-center mb-2">
+            <Image
+              source={{ uri: 'https://placehold.co/30x30' }}
+              style={{ width: 24, height: 24, borderRadius: 12, marginRight: 8 }}
+            />
+            <Text className="text-gray-400">slamer59</Text>
+          </View>
+          <Text className="mb-2 ">dagster_trial</Text>
+          <Text className="mb-2 text-gray-400">Trial for dagster</Text>
+          <View className="flex-row items-center text-gray-400">
+            <FontAwesome5 name="star" size={24} color="yellow" className="mr-1" />
+            <Text className="mr-4">7</Text>
+            <Text>Jupyter Notebook</Text>
+          </View>
+        </View>
+      </View>
+      <View className="mb-4">
+        <View className="flex-row items-center mb-2">
+          <FontAwesome5 name="book" size={24} color="gray" className="mr-2" />
+          <Text className="text-gray-400">Repositories</Text>
+          <Text className="ml-auto ">55</Text>
+        </View>
+        <View className="flex-row items-center mb-2">
+          <FontAwesome5 name="building" size={24} color="gray" className="mr-2" />
+          <Text className="text-gray-400">Organizations</Text>
+          <Text className="ml-auto ">0</Text>
+        </View>
+        <View className="flex-row items-center">
+          <FontAwesome5 name="star" size={24} color="gray" className="mr-2" />
+          <Text className="text-gray-400">Starred</Text>
+          <Text className="ml-auto ">410</Text>
+        </View>
+      </View>
+      <View className="absolute bottom-0 left-0 right-0 flex-row justify-around p-4 bg-gray-900">
+        <FontAwesome5 name="home" size={24} color="white" />
+        <FontAwesome5 name="bell" size={24} color="gray" />
+        <FontAwesome5 name="search" size={24} color="gray" />
+        <FontAwesome5 name="user" size={24} color="blue" />
+      </View>
     </View>
-
   );
 }
-
