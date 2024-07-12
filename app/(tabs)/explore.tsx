817b1517ba3ad4fde4253f6593a76286c447810a
Thomas PEDOT <thomas.pedot@gmail.com>
2024-07-12 16:13:14 +0200

    Remove all for feature

--- app/(tabs)/explore.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from '@/components/Themed';
-
-export default function TabTwoScreen() {
-  return (
-    <View className="items-center justify-center flex-1">
-      <Text className="text-2xl font-bold">Explore To be Done</Text>
-    </View>
-  );
-}
