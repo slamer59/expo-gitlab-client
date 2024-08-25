589742c7a0154077d9d2369bee1e7ab247fc24ed
Thomas PEDOT <thomas.pedot@gmail.com>
2024-08-25 14:40:42 +0200

    fix: token invalidation

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,12 +8,39 @@ export function cn(...inputs: ClassValue[]) {
 
 export async function getToken() {
   let result = await SecureStore.getItemAsync('gitlab-token');
+
   if (result) {
     console.log('Token successfully loaded');
     return result;
   } else {
     console.log('No token stored');
     return null;
+  }
+}
+
+export async function checkValidity(token: string | null) {
+  console.log('Checking token validity...');
+  if (!token) {
+    console.log('No token provided');
+    return false;
+  }
+  try {
+    const response = await fetch('https://gitlab.com/api/v4/user', {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+    if (response.ok) {
+      console.log('Token is valid');
+      return true;
+    } else {
+      console.log('Token is invalid');
+      await resetToken();
+      return false;
+    }
+  } catch (error) {
+    console.error('Error checking token validity:', error);
+    return false;
   }
 }
 
