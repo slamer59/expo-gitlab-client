ffb3c23889a5f789f4c25e0df05c31347f417c2d
Thomas PEDOT <thomas.pedot@gmail.com>
2024-08-25 14:40:42 +0200

    fix: token invalidation

--- app/login.tsx
+++ app/login.tsx
@@ -2,9 +2,8 @@ import * as SecureStore from 'expo-secure-store';
 
 import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
-import { getToken } from '@/lib/utils';
-import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions';
+import { checkValidity, getToken } from '@/lib/utils';
+
 import { useNavigation } from 'expo-router';
 import React, { useEffect, useState } from 'react';
 import { Alert, Linking, TextInput, View } from 'react-native';
@@ -32,75 +31,20 @@ export default function LoginScreen() {
     useEffect(() => {
         checkToken();
     }, []);
-    const mapDeviceToProjectEndpoint = "https://add-device-to-nofitication-et4qi4c73q-uc.a.run.app/"
-    const mapDeviceToProjectURL = `${mapDeviceToProjectEndpoint}/add_device_to_nofitication`
-    const getProjects = async () => {
-        const savedToken = await getToken();
 
-        if (savedToken) {
-            try {
-                const params = {
-                    membership: 'true',
-                };
-
-                const response = await fetch('https://gitlab.com/api/v4/projects' + '?' + new URLSearchParams(params), {
-                    method: 'GET',
-                    headers: {
-                        'PRIVATE-TOKEN': savedToken,
-                    },
-                })
-                    .then((response) => response.json())
-                    .then((projects) => {
-                        console.log('Gitlab API response successful');
-                        return projects;
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                    });
-            } catch (error) {
-                console.error('Error:', error);
-            }
-
-        }
-    }
-    const expoToken = async function getToken() {
-        let { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        if (status !== 'granted') {
-            alert('No notification permissions!');
-            return;
-        }
-        let token = await Notifications.getExpoPushTokenAsync();
-        console.log(token);
-    }
-    const mapDeviceToProject = async () => {
-        console.log('Mapping device to project');
-        const push_token = await expoToken();
-        const projects = await getProjects();
-        console.log(projects);
-        console.log(push_token);
-        fetch(mapDeviceToProjectURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                push_token: push_token,
-                projects: projects
-            })
-        })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error('Error:', error));
-    }
 
 
     const checkToken = async () => {
         console.log('Checking token');
         const savedToken = await getToken();
+        console.log(savedToken);
         if (savedToken) {
-            console.log('Token found, navigating to home');
-            await mapDeviceToProject();
-            navigation.navigate("(tabs)", { screen: "home" });
+            const isValid = await checkValidity(savedToken);
+            console.log(isValid);
+            if (isValid) {
+                console.log('Token found and valid, navigating to home');
+                navigation.navigate("(tabs)", { screen: "home" });
+            }
         }
     };
 
