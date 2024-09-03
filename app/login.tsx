fd61af0c8b48b184de832eff938d8ca09d922f9a
Thomas PEDOT <thomas.pedot@gmail.com>
2024-09-03 21:28:06 +0200

    feat: Use Session for Authentication

--- app/login.tsx
+++ app/login.tsx
@@ -1,145 +1,98 @@
-import * as SecureStore from 'expo-secure-store';
-
 import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
-import { checkValidity, getToken } from '@/lib/utils';
-
+import { useSession } from "@/lib/session/SessionProvider";
 import { useNavigation } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
 import React, { useEffect, useState } from 'react';
-import { Alert, Linking, TextInput, View } from 'react-native';
+import { Alert, Image, Linking, TextInput, View } from 'react-native';
 
-// async function save(key, value) {
-async function save(serverUrl, token) {
-    // await SecureStore.setItemAsync(key, value);
-    await SecureStore.setItemAsync('serverUrl', serverUrl);
-    await SecureStore.setItemAsync('gitlab-token', token);
-}
-
-async function getValueFor(key) {
-    let result = await SecureStore.getItemAsync(key);
-    if (result) {
-        alert("🔐 Here's your value 🔐 \n" + result);
-    } else {
-        alert('No values stored under that key.');
+// Function to validate GitLab URL and PAT
+async function validateGitLabCredentials(url: string, token: string) {
+    try {
+        const response = await fetch(`${url}/api/v4/user`, {
+            headers: { 'PRIVATE-TOKEN': token }
+        });
+        return response.ok;
+    } catch (error) {
+        console.error('Error validating credentials:', error);
+        return false;
     }
 }
 
 export default function LoginScreen() {
-    const [serverUrl, onChangeKey] = useState('https://gitlab.com');
-    const [token, onChangeValue] = useState('');
+    const [url, setUrl] = useState('https://gitlab.com');
+    const [token, setToken] = useState('');
     const navigation = useNavigation();
+    const { signIn, signOut, session, isLoading } = useSession();
+
     useEffect(() => {
-        checkToken();
-    }, []);
-
-    const checkToken = async () => {
-        console.log('Checking token');
-        const savedToken = await getToken();
-
-        if (savedToken) {
-            const isValid = await checkValidity(savedToken);
-            console.log(isValid);
-            if (isValid) {
-                console.log('Token found and valid, navigating to home');
-                navigation.navigate("(tabs)", { screen: "home" });
-            }
+        if (session) {
+            navigation.navigate("(tabs)", { screen: "home" });
         }
-    };
+    }, [session]);
 
-    const handleLogin = async () => {
-        // Store the server URL and token securely
-        if (validateUrl(serverUrl)) {
-            save(serverUrl, token);
-            onChangeKey('https://gitlab.com');
-            onChangeValue('');
-            Alert.alert('Success', 'Server URL and token saved successfully.', [
-                {
-                    text: 'Enter',
-                    onPress: () => {
-                        navigation.navigate("(tabs)", { screen: "home" });
-                    },
-                    style: 'default',
-                },
-            ]
-            )
+    async function handleLogin() {
+        if (!url || !token) {
+            Alert.alert('Error', 'Please enter both URL and token.');
+            return;
+        }
+
+        const isValid = await validateGitLabCredentials(url, token);
+        if (isValid) {
+            await SecureStore.setItemAsync('gitlabUrl', url);
+            await SecureStore.setItemAsync('gitlabToken', token);
+            signIn(url, token);
         } else {
-            console.log(serverUrl)
-            Alert.alert('Error', 'Invalid server URL.');
+            Alert.alert('Error', 'Invalid credentials. Please try again.');
+            setToken('');
         }
-    };
+    }
 
-    const validateUrl = (url) => {
-        try {
-            return Boolean(new URL(url));
-        } catch (e) {
-            return false;
-        }
-    };
+    function openTokenInfoPage() {
+        Linking.openURL('https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html');
+    }
+
+    if (isLoading) {
+        return (
+            <View className="items-center justify-center flex-1">
+                <Text>Loading...</Text>
+            </View>
+        );
+    }
+
     return (
-        <View className="items-center justify-center flex-1 p-2 ">
-            {/* <Image
-                source={require('../../assets/logo.png')}
+        <View className="items-center justify-center flex-1 p-4">
+            <Image
+                source={require('@/assets/logo.png')}
                 style={{ width: 100, height: 100 }}
 
-            /> */}
-            <Text className="mb-6 text-xl font-bold text-center text-blue-600">GitAlchemy: Your GitLab Companion</Text>
-            <View className="w-full px-4 mb-4">
-                <Text className="m-1 text-sm">Server</Text>
+            />
+            <Text className="mb-6 text-xl font-bold text-center text-primaryDark dark:text-dark">GitAlchemy: Your GitLab Companion</Text>
 
-                <TextInput
-                    className="px-4 py-2 bg-gray-200 rounded"
-                    clearTextOnFocus
-                    onChangeText={text => onChangeKey(text)}
-                    value={serverUrl}
-                />
-
-                <Text className="m-1 text-sm text-black">Access Token</Text>
-                {/* <View className="flex-row justify-center mb-4">
-                <TouchableOpacity
-                    className={`px-4 py-2 rounded-l-full bg-gray-200 `}
-                // onPress={ }
-                >
-                    <Text className="text-black">Basic Auth</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    className={`px-4 py-2 rounded-r-full `}
-
-                >
-                    <Text className="text-black">Access Token</Text>
-                </TouchableOpacity>
-            </View> */}
-                <TextInput
-                    className="px-4 py-2 text-black bg-gray-200 rounded"
-                    clearTextOnFocus
-                    onChangeText={text => onChangeValue(text)}
-                    secureTextEntry={true}
-                    placeholder="Enter your GitLab Personal Access Token (PAT)"
-                    value={token}
-                />
-                { }
-                <Button
-                    className="px-4 py-2 m-2 bg-blue-700 rounded"
-                    onPress={() => {
-                        handleLogin();
-                    }}
-                >
-                    <Text>Login</Text>
-                </Button>
-            </View>
-
-            <Text className="text-blue-500">Get Access Token</Text>
-            <Text className="mb-4 text-center text-light dark:text-primaryDark">By signing in you accept our <Text className="text-blue-500" onPress={() => Linking.openURL('#')}>Terms of use</Text> and <Text className="text-blue-500" onPress={() => Linking.openURL('#')}>Privacy policy</Text>.</Text>
-            <Text className="text-blue-500" onPress={() => Linking.openURL('#')}>Trouble signing in?</Text>
-
-
-            {/* <Text style={styles.paragraph}>🔐 Enter your key 🔐</Text>
             <TextInput
-                style={styles.textInput}
-                onSubmitEditing={event => {
-                    getValueFor(event.nativeEvent.text);
-                }}
-                placeholder="Enter the key for the value you want to get"
-            /> */}
-        </View >
+                className="w-full px-3 py-2 mb-4 bg-gray-100 rounded"
+                placeholder="GitLab URL"
+                value={url}
+                onChangeText={setUrl}
+            />
+
+            <TextInput
+                className="w-full px-3 py-2 mb-4 bg-gray-100 rounded"
+                placeholder="Enter your GitLab Personal Access Token (PAT)"
+                value={token}
+                onChangeText={setToken}
+                secureTextEntry
+            />
+
+            <Button onPress={handleLogin} className="w-full mb-4">
+                <Text>Login</Text>
+            </Button>
+
+            <Text className="mb-4 text-blue-500" onPress={openTokenInfoPage}>
+                How to set up a Personal Access Token
+            </Text>
+            <Text className="mb-4 text-center text-light dark:text-primaryDark">By signing in you accept our <Text className="text-blue-500" onPress={() => Linking.openURL('#')}>Terms of use</Text> and <Text className="text-blue-500" onPress={() => Linking.openURL('#')}>Privacy policy</Text>.</Text>
+
+        </View>
     );
 }
