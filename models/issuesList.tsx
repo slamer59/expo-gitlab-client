78dbb4fbea80cb0f0893a8f614ee2a7296fc7f67
Thomas PEDOT <thomas.pedot@gmail.com>
2024-09-04 20:11:51 +0200

    fix: default value list project

--- models/issuesList.tsx
+++ models/issuesList.tsx
@@ -3,26 +3,27 @@ import { APIEntitiesRelatedIssue } from "@/types/general";
 import { Link } from "expo-router";
 
 export function IssuesListComponent({ issues }: { issues: APIEntitiesRelatedIssue[] }) {
+
   return (
     <>
       {issues
         ? issues?.map((issue, index) => (
-            <Link
-              key={index}
-              href={{
-                pathname: "/workspace/projects/[projectId]/issues/[issue_iid]",
-                params: {
-                  projectId: issue.project_id,
-                  issue_iid: issue.iid,
-                },
-              }}
-            >
-              <IssueCard key={index} issue={issue} />
-            </Link>
-          ))
+          <Link
+            key={index}
+            href={{
+              pathname: "/workspace/projects/[projectId]/issues/[issue_iid]",
+              params: {
+                projectId: issue.project_id,
+                issue_iid: issue.iid,
+              },
+            }}
+          >
+            <IssueCard key={index} issue={issue} />
+          </Link>
+        ))
         : Array.from({ length: 5 }).map((_, index) => (
-            <IssueCardSkeleton key={index} />
-          ))}
+          <IssueCardSkeleton key={index} />
+        ))}
     </>
   );
 }
