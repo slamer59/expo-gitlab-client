8cab8ef367d65ef247d805f8aefe0909680b5c7f
Thomas PEDOT <thomas.pedot@gmail.com>
2024-09-05 14:06:52 +0200

    fix: prettier Tab number

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": ["error", { singleQuote: false }],
-    quotes: ["error", "double", { avoidEscape: true }],
-  },
+    extends: ["expo", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                printWidth: 80,
+                tabWidth: 4,
+                semi: true,
+                singleQuote: false,
+                trailingComma: "es5",
+                bracketSpacing: true,
+                jsxBracketSameLine: false,
+            },
+        ],
+    },
 };
