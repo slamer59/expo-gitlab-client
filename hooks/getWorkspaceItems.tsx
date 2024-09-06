fe8b0cf13214884080b3cf3a024a84e9863a9670
Thomas PEDOT <thomas.pedot@gmail.com>
2024-09-06 21:49:05 +0200

    refactor: workspaceItems

--- hooks/getWorkspaceItems.tsx
+++ hooks/getWorkspaceItems.tsx
@@ -14,16 +14,16 @@ export function getWorspaceItems(project: IProject, router: string[]): IListItem
         {
             icon: "alert-circle-outline",
             text: "Issues",
-            kpi: project.repository?.open_issues_count || 0,
+            kpi: project?.repository?.open_issues_count || 0,
             onAction: () => router.push(`workspace/projects/${project.repository.id}/issues/list`),
             itemColor: "#3de63d",
         },
         {
             icon: "git-merge",
             text: "Merge Requests",
-            kpi: project.mergeRequest ? project.mergeRequest.length : 0,
+            kpi: project?.mergeRequest ? project?.mergeRequest.length : 0,
             onAction: () => router.push(
-                `workspace/projects/${project.repository.id}/merge-requests/list`
+                `workspace/projects/${project?.repository.id}/merge-requests/list`
             ),
             itemColor: "#3e64ed",
         },
@@ -32,29 +32,29 @@ export function getWorspaceItems(project: IProject, router: string[]): IListItem
             text: "CI/CD",
             kpi: "",
             onAction: () => router.push(
-                `workspace/projects/${project.repository.id}/pipelines/list`
+                `workspace/projects/${project?.repository.id}/pipelines/list`
             ),
             itemColor: "#d5ea4e",
         },
         {
             icon: "people-circle-outline",
             text: "Members",
-            kpi: project.members.length || 0,
-            onAction: () => router.push(`workspace/projects/${project.repository.id}/members/list`),
+            kpi: project?.members.length || 0,
+            onAction: () => router.push(`workspace/projects/${project?.repository.id}/members/list`),
         },
         {
             icon: "document-text-outline",
             text: "Licences",
             kpi: "",
             onAction: () => router.push(
-                `workspace/projects/${project.repository.id}/licences/list`
+                `workspace/projects/${project?.repository.id}/licences/list`
             ),
         },
         {
             icon: "star-outline",
             text: "Starred",
-            kpi: project.repository?.star_count || "",
-            onAction: () => router.push(`workspace/projects/${project.repository.id}/starred/list`),
+            kpi: project?.repository?.star_count || "",
+            onAction: () => router.push(`workspace/projects/${project?.repository.id}/starred/list`),
         },
     ];
 }
