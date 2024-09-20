ab2ce465fe490f9674b5733a365ba1f04843d3a5
Thomas PEDOT <thomas.pedot@gmail.com>
2024-09-20 16:52:28 +0200

    WIP: Pipeline Card

---
+++ components/Pipeline/pipeline-card.tsx
@@ -0,0 +1,55 @@
+import { formatDate } from "@/lib/utils";
+
+import React from "react";
+import { Text, View } from "react-native";
+import { Pills } from "../Pills";
+import { Skeleton } from "../ui/skeleton";
+import PipelineStatusIcon from "./pipeline-status-icon";
+
+export function PipelineCardSkeleton() {
+    return (
+        <View className="flex-row items-center p-4 my-2 space-x-4 rounded-lg bg-card">
+            <Skeleton className="w-12 h-12 m-2 space-x-4 rounded-full" />
+            <View className="flex-1 space-y-2">
+                <Skeleton className="w-full h-4 mb-2" />
+                <Skeleton className="w-3/4 h-4" />
+            </View>
+        </View>
+    );
+}
+
+export function PipelineCard({ item }) {
+    console.log("PipelineCard item:", item);
+    return (
+        <View className="flex-row p-3 mt-2 mb-2 rounded-lg bg-card">
+            <View className="mr-2">
+                {PipelineStatusIcon(item, false)}
+            </View>
+            <View className="flex-1 mt-1">
+                <View className="flex-row items-center justify-between mb-1">
+                    <Text className="text-sm text-muted">{item.references.full}</Text>
+                    <Text className="text-sm text-muted">{formatDate(item.updated_at)}</Text>
+                </View>
+                <Text className="mb-2 text-lg font-bold text-white" testID={`issue-card`}>{item.title}</Text>
+                <View className="flex-row items-center space-x-2">
+                    {item?.labels.length > 0 && (
+                        <View className="flex-row flex-wrap">
+                            {item?.labels.map((label, index) => (
+                                <Pills
+                                    key={index}
+                                    label={label}
+                                    variant="purple"
+                                />
+                            ))}
+                        </View>
+                    )}
+                    {/* Comments */}
+                    {/* <View className="flex-row items-center">
+              {PipelineStatusIcon(item, false)}
+              <Text className="ml-1 text-sm text-gray-400">2</Text>
+            </View> */}
+                </View>
+            </View>
+        </View>
+    );
+}
