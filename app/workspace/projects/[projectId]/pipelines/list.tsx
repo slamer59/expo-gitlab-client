3aae33a49989538fbaeb4a217bc26ecb4702e9f7
Thomas PEDOT <thomas.pedot@gmail.com>
2024-09-20 16:52:28 +0200

    WIP: Pipeline Card

---
+++ app/workspace/projects/[projectId]/pipelines/list.tsx
@@ -0,0 +1,45 @@
+import ListWithFilters from "@/components/ListWithFilters";
+import { PipelineCard, PipelineCardSkeleton } from "@/components/Pipeline/pipeline-card";
+import { GlobalMergeRequestUIFilters } from "@/constants/UIFilters";
+
+import { Stack, useLocalSearchParams } from "expo-router";
+import { ScrollView } from "react-native";
+
+export default function ProjectMergeRequestsList() {
+    const { projectId } = useLocalSearchParams();
+    const defaultParamsProjectPR = {
+        path: {
+            id: projectId,
+        },
+        query: {
+            // ... (previous query options remain unchanged)
+        }
+    };
+    const UIFilters = GlobalMergeRequestUIFilters
+    const query_cache_name = `project_id_pipelines_${projectId}`
+    const pathname = "/workspace/projects/[projectId]/pipelines"
+    const endpoint = "/api/v4/projects/{id}/pipelines"
+    const paramsMap = {
+        "projectId": "project_id"
+    }
+
+    return (
+        <ScrollView className="flex-1 p-2 bg-background">
+            <Stack.Screen
+                options={{
+                    headerTitle: `Pipeline for Project`,
+                }}
+            />
+            <ListWithFilters
+                UIFilters={UIFilters}
+                ItemComponent={PipelineCard}
+                SkeletonComponent={PipelineCardSkeleton}
+                pathname={pathname}
+                endpoint={endpoint}
+                query_cache_name={query_cache_name}
+                paramsMap={paramsMap}
+                defaultParams={defaultParamsProjectPR}
+            />
+        </ScrollView>
+    );
+}
---
+++ components/Pipeline/pipeline-status-icon.tsx
@@ -0,0 +1,33 @@
+import { Text } from "@/components/ui/text";
+import { Ionicons } from '@expo/vector-icons';
+
+export default function PipelineStatusIcon(issue: any, withText: boolean = false) {
+    return <>
+        {issue.state === 'opened' ? (
+            <>
+                <Ionicons name="checkmark-circle" size={24} color="green" />
+                {withText && <Text className='ml-1 text-green-500'>Open</Text>}
+            </>
+        ) : issue.state === 'closed' ? (
+            <>
+                <Ionicons name="close-circle" size={24} color="red" />
+                {withText && <Text className='ml-1 text-red-500'>Closed</Text>}
+            </>
+        ) : issue.state === 'locked' ? (
+            <>
+                <Ionicons name="lock-closed" size={24} color="orange" />
+                {withText && <Text className='ml-1 text-orange-500'>Locked</Text>}
+            </>
+        ) : issue.state === 'merged' ? (
+            <>
+                <Ionicons name="git-branch" size={24} color="purple" />
+                {withText && <Text className='ml-1 text-purple-500'>Merged</Text>}
+            </>
+        ) : (
+            <>
+                <Ionicons name="help-circle" size={24} color="blue" />
+                {withText && <Text className='ml-1 text-blue-500'>Unknown</Text>}
+            </>
+        )}
+    </>;
+}
