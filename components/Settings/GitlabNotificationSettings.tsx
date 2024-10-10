b35f2380fd553347fff3335d02dc65a345c1328f
Thomas PEDOT <thomas.pedot@gmail.com>
2024-10-10 18:12:25 +0200

    feat: Update notification UI with real state from gitlab

--- components/Settings/GitlabNotificationSettings.tsx
+++ components/Settings/GitlabNotificationSettings.tsx
@@ -1,45 +1,94 @@
 import { Ionicons } from '@expo/vector-icons';
-import React, { useState } from 'react';
-import { Modal, Text, TouchableOpacity, View } from 'react-native';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Modal, ScrollView, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+import { Checkbox } from '../ui/checkbox';
 import { Separator } from '../ui/separator';
 
 const notificationLevels = [
     { value: 'global', label: 'Global', description: 'Use your global notification setting', icon: 'globe' },
     { value: 'watch', label: 'Watch', description: 'You will receive notifications for any activity', icon: 'eye' },
-    { value: 'participate', label: 'Participate', description: 'You will only receive notifications for issues you have participated in', icon: 'chatbubbles' },
+    { value: 'participating', label: 'Participate', description: 'You will only receive notifications for issues you have participated in', icon: 'chatbubbles' },
     { value: 'mention', label: 'On mention', description: 'You will receive notifications only for @mentions', icon: 'at' },
     { value: 'disabled', label: 'Disabled', description: 'You will not receive any notifications', icon: 'notifications-off' },
-    { value: 'custom', label: 'Custom', description: 'You will receive notifications based on your custom settings', icon: 'settings' },
+    // { value: 'custom', label: 'Custom', description: 'You will receive notifications based on your custom settings', icon: 'settings' },
 ];
 
 export default function NotificationDashboard() {
-    const [groups, setGroups] = useState([
-        { name: 'ecommerce', level: notificationLevels[0] },
-        { name: 'expo-app', level: notificationLevels[0] },
-        { name: 'infrastructure', level: notificationLevels[0] },
-        { name: 'Jokosun', level: notificationLevels[0] },
-        { name: 'outils', level: notificationLevels[0] },
-        { name: 'Plateforme investisseurs', level: notificationLevels[0] },
-        { name: 'playground', level: notificationLevels[0] },
-    ]);
-
-    const [projects, setProjects] = useState([
-        { name: 'Thomas PEDOT / fake', level: notificationLevels[1] },
-        { name: 'Thomas PEDOT / fake-gitlab', level: notificationLevels[1] },
-        { name: 'Jokosun / vitrine', level: notificationLevels[3] },
-        { name: 'Jokosun / jokosun e-commerce', level: notificationLevels[3] },
-        { name: 'Jokosun / component-library', level: notificationLevels[0] },
-        { name: 'Jokosun / jokosun-wordpress', level: notificationLevels[0] },
-        { name: 'Thomas PEDOT / expo-gitlab-client', level: notificationLevels[0] },
-        { name: 'Thomas PEDOT / gemseo', level: notificationLevels[0] },
-        { name: 'Thomas PEDOT / obsidian-template', level: notificationLevels[0] },
-        { name: 'Thomas PEDOT / Obsidian Vault', level: notificationLevels[0] },
-    ]);
-
+    const [groups, setGroups] = useState([]);
+    const [projects, setProjects] = useState([]);
     const [globalNotificationLevel, setGlobalNotificationLevel] = useState(notificationLevels[0]);
     const [modalVisible, setModalVisible] = useState(false);
     const [selectedItemType, setSelectedItemType] = useState('');
     const [selectedItemIndex, setSelectedItemIndex] = useState(-1);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isChecked, setIsChecked] = useState(false);
+
+
+    // Get all info from axios 
+
+    const API_BASE_URL = 'https://gitlab.com/api/v4';
+    const ACCESS_TOKEN = 'GITLAB_PAT_REMOVED';
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const fetchNotificationSettings = async (type, id) => {
+                    console.log("🚀 ~ fetchNotificationSettings ~ id:", id)
+                    try {
+                        const response = await axios.get(`${API_BASE_URL}/${type}s/${id}/notification_settings`, {
+                            headers: { 'Authorization': `Bearer ${ACCESS_TOKEN}` }
+                        });
+                        console.log("🚀 ~ fetchNotificationSettings ~ response:", response.data.level)
+
+                        return notificationLevels.find(level => level.value === response.data.level) || notificationLevels[0];
+                    } catch (error) {
+                        console.error(`Error fetching ${type} notification settings:`, error);
+                        return notificationLevels[0];
+                    }
+                };
+
+                // Fetch groups
+                const groupsResponse = await axios.get(`${API_BASE_URL}/groups`, {
+                    headers: { 'Authorization': `Bearer ${ACCESS_TOKEN}` }
+                });
+                const groupsWithSettings = await Promise.all(groupsResponse.data.map(async group => ({
+                    id: group.id,
+                    name: group.name,
+                    level: await fetchNotificationSettings('group', group.id)
+                })));
+                console.log("🚀 ~ groupsWithSettings ~ groupsWithSettings:", groupsWithSettings)
+                setGroups(groupsWithSettings);
+
+                // Fetch projects
+                const projectsResponse = await axios.get(`${API_BASE_URL}/projects`, {
+                    params: { membership: true, owned: true },
+                    headers: { 'Authorization': `Bearer ${ACCESS_TOKEN}` }
+                });
+                const projectsWithSettings = await Promise.all(projectsResponse.data.map(async project => ({
+                    id: project.id,
+                    name: project.path_with_namespace,
+                    level: await fetchNotificationSettings('project', project.id)
+                })));
+                setProjects(projectsWithSettings);
+
+                // Fetch global notification settings
+                const globalSettingsResponse = await axios.get(`${API_BASE_URL}/notification_settings`, {
+                    headers: { 'Authorization': `Bearer ${ACCESS_TOKEN}` }
+                });
+                setGlobalNotificationLevel(notificationLevels.find(level => level.value === globalSettingsResponse.data.level) || notificationLevels[0]);
+
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     const openModal = (type, index = -1) => {
         setSelectedItemType(type);
@@ -61,7 +110,14 @@ export default function NotificationDashboard() {
         }
         setModalVisible(false);
     };
-
+    if (isLoading) {
+        return (
+            <View className="items-center justify-center flex-1">
+                <ActivityIndicator size="large" color="#0000ff" />
+                <Text className="mt-2 text-white">Loading...</Text>
+            </View>
+        );
+    }
     return (
         <>
             <View className="p-4 m-1 rounded-lg bg-card">
@@ -84,26 +140,45 @@ export default function NotificationDashboard() {
                         <Text className="text-white">{globalNotificationLevel.label}</Text>
                         <Ionicons name="chevron-down" size={18} color="#fff" />
                     </TouchableOpacity>
+                    <View className='flex-row items-center'>
+                        <Checkbox
+                            className="flex-row items-center"
+                            checked={isChecked}
+                            onCheckedChange={setIsChecked}
+                        />
+                        <Text className="ml-2 text-white">Owned activity ?</Text>
+                    </View>
                 </View>
                 <Separator className="my-4 bg-secondary" />
                 <View className="mb-6">
                     <Text className="mb-2 text-xl font-bold text-white">Groups ({groups.length})</Text>
                     {groups.map((group, index) => (
-                        <View key={index} className="flex-row items-center justify-between py-3 border-b border-muted">
-                            <View className="flex-row items-center">
-                                <Ionicons
-                                    name={`notifications${group.level.value === 'disabled' ? '-off' : ''}`}
-                                    size={18}
-                                    color="#fff"
-                                />
-                                <Text className="ml-2 text-white">{group.name}</Text>
+                        <ScrollView
+                            horizontal
+                            className="mb-2 ml-4 bg-transparent max-h-16"
+                            contentContainerStyle={{ flexGrow: 1 }}
+                        >
+                            <View key={index} className="flex-row items-center justify-between w-full py-3 border-b border-muted">
+                                <View className="flex-row items-center flex-1 mr-4">
+                                    <Ionicons
+                                        name={`notifications${group.level.value === 'disabled' ? '-off' : ''}`}
+                                        size={18}
+                                        color="#fff"
+                                    />
+                                    <Text className="ml-2 text-white" numberOfLines={1} ellipsizeMode="tail">
+                                        {group.name}
+                                    </Text>
+                                </View>
+                                <TouchableOpacity
+                                    className="flex-row items-center p-2 rounded-md bg-muted"
+                                    onPress={() => openModal('group', index)}
+                                >
+                                    <Ionicons name={group.level.icon} size={18} color="#fff" />
+                                    <Text className="mx-1 text-white">{group.level.label}</Text>
+                                    <Ionicons name="chevron-down" size={18} color="#fff" />
+                                </TouchableOpacity>
                             </View>
-                            <TouchableOpacity className="flex-row items-center p-2 rounded-md bg-muted" onPress={() => openModal('group', index)}>
-                                <Ionicons name={group.level.icon} size={18} color="#fff" />
-                                <Text className="mx-1 text-white">{group.level.label}</Text>
-                                <Ionicons name="chevron-down" size={18} color="#fff" />
-                            </TouchableOpacity>
-                        </View>
+                        </ScrollView>
                     ))}
                 </View>
                 <Separator className="my-4 bg-secondary" />
@@ -111,21 +186,35 @@ export default function NotificationDashboard() {
                     <Text className="mb-2 text-xl font-bold text-white">Projects ({projects.length})</Text>
                     <Text className="mb-3 text-muted">To specify the notification level per project of a group you belong to, visit the project page and change the notification level there.</Text>
                     {projects.map((project, index) => (
-                        <View key={index} className="flex-row items-center justify-between py-3 border-b border-muted">
-                            <View className="flex-row items-center">
-                                <Ionicons
-                                    name={`notifications${project.level.value === 'disabled' ? '-off' : ''}`}
-                                    size={18}
-                                    color="#fff"
-                                />
-                                <Text className="ml-2 text-white">{project.name}</Text>
+                        <ScrollView
+                            horizontal
+                            className="mb-2 ml-4 bg-transparent max-h-16"
+                            contentContainerStyle={{ flexGrow: 1 }}
+                        >
+                            <View className="flex-row items-center justify-between w-full py-3 border-b border-muted">
+                                <View className="flex-row items-center flex-1 mr-4">
+                                    <Ionicons
+                                        name={`notifications${project.level.value === 'disabled' ? '-off' : ''}`}
+                                        size={18}
+                                        color="#fff"
+                                    />
+                                    <Text className="ml-2 text-white" numberOfLines={1} ellipsizeMode="tail">
+                                        {project.name}
+                                    </Text>
+                                </View>
+                                <TouchableOpacity
+                                    className="flex-row items-center p-2 rounded-md bg-muted"
+                                    onPress={() => openModal('project', index)}
+                                >
+                                    <Ionicons name={project.level.icon} size={18} color="#fff" />
+                                    <Text className="mx-1 text-white">{project.level.label}</Text>
+                                    <Ionicons name="chevron-down" size={18} color="#fff" />
+                                </TouchableOpacity>
                             </View>
-                            <TouchableOpacity className="flex-row items-center p-2 rounded-md bg-muted" onPress={() => openModal('project', index)}>
-                                <Ionicons name={project.level.icon} size={18} color="#fff" />
-                                <Text className="mx-1 text-white">{project.level.label}</Text>
-                                <Ionicons name="chevron-down" size={18} color="#fff" />
-                            </TouchableOpacity>
-                        </View>
+                        </ScrollView>
+
+
+
                     ))}
                 </View>
             </View>
@@ -136,24 +225,24 @@ export default function NotificationDashboard() {
                 visible={modalVisible}
                 onRequestClose={() => setModalVisible(false)}
             >
-                <View className="justify-end flex-1 bg-black bg-opacity-50">
-                    <View className="p-5 bg-card rounded-t-2xl">
-                        <Text className="mb-4 text-2xl font-bold text-white">Select Notification Level</Text>
-                        {notificationLevels.map((level, index) => (
-                            <TouchableOpacity
-                                key={index}
-                                className="py-3 border-b border-muted"
-                                onPress={() => selectNotificationLevel(level)}
-                            >
-                                <Text className="mb-1 text-xl font-bold text-white">{level.label}</Text>
-                                <Text className="text-muted">{level.description}</Text>
-                            </TouchableOpacity>
-                        ))}
-                        <TouchableOpacity className="p-3 mt-4 rounded-lg bg-popover" onPress={() => setModalVisible(false)}>
-                            <Text className="text-xl font-bold text-white">Close</Text>
-                        </TouchableOpacity>
+                <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+
+                    <View className="justify-end flex-1 bg-black bg-opacity-50">
+                        <View className="p-5 bg-card rounded-t-2xl">
+                            <Text className="mb-4 text-4xl font-bold text-white">Select Notification Level</Text>
+                            {notificationLevels.map((level, index) => (
+                                <TouchableOpacity
+                                    key={index}
+                                    className="py-3 border-b border-muted"
+                                    onPress={() => selectNotificationLevel(level)}
+                                >
+                                    <Text className="mb-1 text-xl font-bold text-white">{level.label}</Text>
+                                    <Text className="text-muted">{level.description}</Text>
+                                </TouchableOpacity>
+                            ))}
+                        </View>
                     </View>
-                </View>
+                </TouchableWithoutFeedback>
             </Modal>
         </>
     );
