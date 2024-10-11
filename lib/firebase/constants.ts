79a62da692f2da02cb7f6c3e82df14efcc33b321
Thomas PEDOT <thomas.pedot@gmail.com>
2024-10-11 14:48:05 +0200

    fix: Remove first attempt for notification

--- lib/firebase/constants.ts
+++ lib/firebase/constants.ts
@@ -1,5 +1,4 @@
 
 export const mapDeviceToProjectEndpoint = "https://add-device-to-nofitication-et4qi4c73q-uc.a.run.app";
-export const mapDeviceToProjectURL = `${mapDeviceToProjectEndpoint}/add_device_to_nofitication`;
 export const webhookName = "WEBHOOK_GITLAB_GITALCHEMY"
 export const webhooksUrl = "https://webhook-gitlab-et4qi4c73q-uc.a.run.app/webhook_gitlab"
--- lib/firebase/helpers.ts
+++ lib/firebase/helpers.ts
@@ -1,25 +1,3 @@
-import { mapDeviceToProjectURL } from "./constants";
-
-export async function mapDeviceToProject(push_token: any, projects: { http_url_to_repo: string }): Promise<any> {
-    console.log("Mapping device to project");
-    const response = await fetch(mapDeviceToProjectURL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            push_token: push_token,
-            projects: projects.map(project => project.http_url_to_repo),
-        }),
-    });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    console.log("Map device to project done");
-    return "Map device to project done"
-}
-
 export const firebaseConfig = {
     apiKey: "FIREBASE_API_KEY_REMOVED",
     authDomain: "gitalchemy.firebaseapp.com",
