2992854c239d17d01301f3eef81d9cf6f9ce5d8b
Thomas PEDOT <thomas.pedot@gmail.com>
2024-10-11 16:50:51 +0200

    fix: job pipeline and list pipelines

---
+++ app/workspace/projects/[projectId]/jobs/index.tsx
@@ -0,0 +1,8 @@
+import NotImplemented from '@/app/not-implemented';
+import React from 'react';
+
+export default function JobDetails() {
+    return (
+        <NotImplemented message="Job Detail Page" />
+    );
+}
