0581ec815343180dd093d8e6c77a60733bbbfa84
Thomas PEDOT <thomas.pedot@gmail.com>
2024-10-14 21:05:57 +0200

    WIP: Add react native syntax highlighter

--- app/tree/[projectId]/[fileId].tsx
+++ app/tree/[projectId]/[fileId].tsx
@@ -1,14 +1,16 @@
-import MarkdownCustom from "@/components/CustomMarkdown";
+import { HeaderAction, HeaderOption, HeaderRight } from "@/components/HeaderRight";
 import Loading from "@/components/Loading";
 import { Text } from "@/components/ui/text";
 import { useGetData } from '@/lib/gitlab/hooks';
-import { styles } from "@/lib/markdown-styles";
 import { Image } from 'expo-image';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { Dimensions, ScrollView, View } from 'react-native';
 import * as mime from 'react-native-mime-types';
+import SyntaxHighlighter from 'react-native-syntax-highlighter';
+/*by default component uses hljs so access hljs styles, import from /prism for prism styles */
 
+import { a11yDark as highlightStyle } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const decodeBase64 = (str) => {
     try {
@@ -19,39 +21,91 @@ const decodeBase64 = (str) => {
     }
 };
 
+const getFileType = (fileName: string): string => {
+    const extension = fileName.split('.').pop()?.toLowerCase() || '';
 
-const Component = ({ mimeType, content }) => {
-    console.log('MIME Type:', mimeType);
-
-    // console.log('Content:', content);
-    let contentComponent;
-    const decodedContent = content ? decodeBase64(content) : 'Cannot read this file yet';
-    // console.log("🚀 ~ Component ~ decodedContent:", decodedContent)
-
-    switch (mimeType) {
-        case 'text/yaml':
-        case 'text/x-yaml':
-        case 'application/json':
-        case 'text/markdown':
-        case 'text/plain':
-            contentComponent = <MarkdownCustom
-                className="mb-4"
-                style={styles}
-
-            >{decodedContent}</MarkdownCustom>;
-            break;
-        case 'image/jpeg':
-        case 'image/png':
-        case 'image/gif':
-            const { width } = Dimensions.get('window');
-            contentComponent = <Image source={{ uri: `data:image/png;base64,${content}` }} style={{ width, height: width }} />
-            break;
+    switch (extension) {
+        case 'js':
+        case 'jsx':
+            return 'javascript';
+        case 'tsx':
+        case 'ts':
+            return 'typescript';
+        case 'py':
+            return 'python';
+        case 'rb':
+            return 'ruby';
+        case 'java':
+            return 'java';
+        case 'cpp':
+        case 'c':
+            return 'c';
+        case 'html':
+            return 'html';
+        case 'css':
+            return 'css';
+        case 'php':
+            return 'php';
+        case 'go':
+            return 'go';
+        case 'rust':
+        case 'rs':
+            return 'rust';
+        case 'swift':
+            return 'swift';
+        case 'kt':
+        case 'kts':
+            return 'kotlin';
+        case 'scala':
+            return 'scala';
+        case 'sh':
+        case 'bash':
+            return 'bash';
+        case 'yml':
+        case 'yaml':
+            return 'yaml';
+        case 'json':
+            return 'json';
+        case 'md':
+            return 'markdown';
+        case 'sql':
+            return 'sql';
+        case 'txt':
+            return 'text';
         default:
-            contentComponent = <Text>Cannot read this file yet</Text>;
+            return 'text';
     }
-    return contentComponent;
+};
 
-}
+
+const Component = ({ mimeType, fileType, content }) => {
+    console.log('MIME Type:', mimeType);
+    console.log('File Type:', fileType);
+
+    const decodedContent = content ? decodeBase64(content) : 'Cannot read this file yet';
+
+    let contentComponent;
+
+    if (mimeType && mimeType.startsWith('image/')) {
+        const { width } = Dimensions.get('window');
+        contentComponent = <Image source={{ uri: `data:${mimeType};base64,${content}` }} style={{ width, height: width }} />;
+    } else if (fileType !== 'text') {
+        contentComponent = <SyntaxHighlighter
+            language='javascript'
+            style={highlightStyle}
+            highlighter={"prism" || "hljs"}
+        >
+            {decodedContent}
+        </SyntaxHighlighter>;
+
+    } else {
+        contentComponent = <Text>{decodedContent}</Text>;
+    }
+
+    return contentComponent
+};
+
+
 
 export default function FileView() {
     const { projectId, path, ref } = useLocalSearchParams();
@@ -74,24 +128,55 @@ export default function FileView() {
     if (isError) return <Text>Error fetching data</Text>;
 
 
-    const { content, commit, file_path, file_name, encoding } = file || {};
-    // console.log('File:', content);
-    return (
 
+    const { content, commit, file_path, file_name, encoding } = file || {};
+
+    const mimeType = mime.lookup(file_name) || 'text/plain';
+    const fileType = getFileType(file_name);
+
+    const headerActions: HeaderAction[] = [
+        {
+            icon: "share-social-outline",
+            onPress: () => console.log("To Be Implemented"), // shareView(issue?.web_url),
+            testID: "share-issue-button"
+        }
+    ];
+
+    const headerOptions: HeaderOption[] = [
+        // Choose theme 
+        {
+            icon: "color-palette-outline",
+            label: "Choose theme",
+            onPress: () => console.log("To Be Implemented"), // shareView(issue?.web_url),
+            testID: "choose-theme-button"
+        }
+    ];
+
+    return (
         <ScrollView
-            className="flex-1 p-2 bg-card"
+            className="flex-1 p-4 bg-card"
             contentContainerStyle={{ paddingBottom: 100 }}
         >
             <Stack.Screen
                 options={{
-                    title: file_name
-                    // ...defaultOptionsHeader
+                    title: file_name,
+                    headerRight: () => (
+                        <HeaderRight
+                            actions={headerActions}
+                            options={headerOptions}
+                        />
+                    )
                 }}
             />
+            {/* <Text className="mb-2 text-lg font-bold">File Path: {file_path}</Text>
+            <Text className="mb-2">MIME Type: {mimeType}</Text>
+            <Text className="mb-2">File Type: {fileType}</Text> */}
+
             {isLoading ? <Loading />
                 :
                 <Component
-                    mimeType={mime.lookup(file_name)}
+                    mimeType={mimeType}
+                    fileType={fileType}
                     content={content}
                 />}
             <View className='flex-row h-6'></View>
